4abbe8f556416bb2a694e8753433ec03ecda860f
William W Wold <wilcoze@gmail.com>
2017-07-06 22:58:48 -0700

    added argument counting macro functionality

--- cpp_macro_impl/main.cpp
+++ cpp_macro_impl/main.cpp
@@ -2,4 +2,10 @@
 
 int main() {
 	print("hay");
+	
+	//var a = tuple(a, 54, dsklgs, true);
+	
+	//print(a.true);
+	
+	print(ARG_COUNT(dfsd, sdfds, sfsd, sf));
 }
--- cpp_macro_impl/skylight_macro.h
+++ cpp_macro_impl/skylight_macro.h
@@ -1,5 +1,13 @@
 #include <iostream>
 
+// helpers
+
+#define ARG_COUNT(...) ARG_COUNT_INTENAL(dummy, ##__VA_ARGS__, 48,47,46,45,44,43,42,41,40,39,38,37,36,35,34,33,32,31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0)
+
+#define IS_EMPTY(...) ARG_COUNT_INTENAL(dummy, ##__VA_ARGS__, args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,args,empty)
+
+#define ARG_COUNT_INTENAL(_0,_1,_2,_3,_4,_5,_6,_7,_8,_9,_10,_11,_12,_13,_14,_15,_16,_17,_18,_19,_20,_21,_22,_23,_24,_25,_26,_27,_28,_29,_30,_31,_32,_33,_34,_35,_36,_37,_38,_39,_40,_41,_42,_43,_44,_45,_46,_47,_48,N,...) N
+
 #define var auto
 
 #define print(arg) std::cout << arg << std::endl
@@ -18,5 +26,16 @@
 #define _3 FUNC_3_ARGS(
 
 
-#define TUPLE_ELEM(e_name, e_val) decltype(e_val) e_name = e_val;
-#define tuple(name0, val0, name1, val1) [&]()->auto{struct anon_struct{TUPLE_ELEM(name0, val0) TUPLE_ELEM(name1, val1)}; return anon_struct();}()
+//#define TUPLE_ELEM(e_name, e_val) decltype(e_val) e_name = e_val;
+
+//#define TUPLE_INSIDE(first_name, first_val, ...) TUPLE_ELEM(first_name, first_val) TUPLE_INSIDE(__VA_ARGS__)
+
+//#define tuple(...) [&]() -> auto { struct anon_struct{TUPLE_INSIDE(__VA_ARGS__)}; return anon_struct();}()
+
+//#define tuple(name0, val0) [&]()->auto{struct anon_struct{ \
+	TUPLE_ELEM(name0, val0) \
+	}; return anon_struct();}()
+//#define tuple(name0, val0, name1, val1) [&]()->auto{struct anon_struct{ \
+	TUPLE_ELEM(name0, val0) \
+	TUPLE_ELEM(name1, val1) \
+	}; return anon_struct();}()
